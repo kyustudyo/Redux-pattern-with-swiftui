020de39668795f3eb5d0c4facf6bc85278ec5c8a
LeeHankyu <2014272050@yonsei.ac.kr>
2022-05-08 12:51:48 +0900

    Initial Commit

---
+++ Redux pattern/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Redux pattern
+//
+//  Created by Hankyu Lee on 2022/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Redux pattern/Redux_patternApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redux_patternApp.swift
+//  Redux pattern
+//
+//  Created by Hankyu Lee on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct Redux_patternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
