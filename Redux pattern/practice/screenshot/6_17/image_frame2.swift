9f57e00414e2940a51d2c8482c2549f1fdad03f4
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-06-17 20:51:31 +0900

    image frame practice

---
+++ Redux pattern/practice/screenshot/6_17/image_frame2.swift
@@ -0,0 +1,25 @@
+//
+//  image_frame2.swift
+//  Redux pattern
+//
+//  Created by Hankyu Lee on 2022/06/17.
+//
+
+import SwiftUI
+
+struct image_frame2: View {
+    var body: some View {
+
+        Image("1")
+            .resizable()
+
+                        
+            
+    }
+}
+
+struct image_frame2_Previews: PreviewProvider {
+    static var previews: some View {
+        image_frame2()
+    }
+}
