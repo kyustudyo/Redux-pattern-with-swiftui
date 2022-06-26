6cfc6ac670e21e0f0646078826a5bfd702c100cc
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-06-26 19:41:50 +0900

    6_26 State, Observable - struct, class

--- Redux pattern/practice/screenshot/6_24/lazy_property.playground/Contents.swift
+++ Redux pattern/practice/screenshot/6_24/lazy_property.playground/Contents.swift
@@ -11,7 +11,6 @@ let images = indexes.lazy.filter { index -> Bool in
     print("[Map]")
     return "image_\(index)"
 }
-
 let lastThreeImages = images.suffix(3)
 for img in lastThreeImages {
     print(img)
