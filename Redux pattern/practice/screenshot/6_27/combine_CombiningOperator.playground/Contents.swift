b6b24894b1d362bcb05bb1a58eb949b90a2d9ec0
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-06-28 15:09:40 +0900

    6_28 combine combining

--- Redux pattern/practice/screenshot/6_27/combine_CombiningOperator.playground/Contents.swift
+++ Redux pattern/practice/screenshot/6_27/combine_CombiningOperator.playground/Contents.swift
@@ -40,5 +40,90 @@ publishers.send(publisher2)// 바꾸기. 마지막으로 들어온 애한테 집
 publisher2.send("????")
 publisher1.send("!!!!!")// x
 
+// 패스안에 패스
+//let publisher3 = PassthroughSubject<String, Never>()
+//publisher3.switchToLastest() //자체가 안됨
+
+
+print("ㅡㅡㅡㅡImages 실험ㅡㅡㅡㅡ")
+let images = ["1","2","3"]
+var index = 0
+
+func getImage() -> AnyPublisher<UIImage?, Never> {
+    return
+    Future<UIImage?, Never> { promise in
+        DispatchQueue.global().asyncAfter(deadline: .now() + 3.0) {
+            promise(.success(UIImage(named: images[index])))
+        }
+    }
+    .print()
+    .map { $0 }
+        .receive(on: RunLoop.main)
+        .eraseToAnyPublisher()
+}
+
+let taps = PassthroughSubject<Void, Never>()
+
+let subscription = taps.map { _ in getImage() }
+    .print()
+    .switchToLatest().sink {
+        print($0)
+}
+
+taps.send()
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 6.0) {
+    index += 1
+    taps.send()
+}
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 6.5) {
+    index += 1
+    taps.send()
+}
+
+
+print("ㅡㅡㅡㅡMergeㅡㅡㅡㅡ")
+//두개다 받고싶을때
+let publisher4 = PassthroughSubject<Int, Never>()
+let publisher5 = PassthroughSubject<Int, Never>()
+
+publisher4.merge(with: publisher5).sink {
+    print($0)
+}
+
+publisher4.send(10)
+publisher4.send(20)
+publisher5.send(30)
+publisher5.send(40)
+
+
+print("ㅡㅡㅡㅡCombineLatestㅡㅡㅡㅡ")
+
+let publisher10 = PassthroughSubject<Int, Never>()
+let publisher11 = PassthroughSubject<String, Never>()
+
+publisher10.combineLatest(publisher11)
+    .sink {
+        print("1:\($0), 2:\($1)")
+    }
+publisher10.send(1)
+publisher11.send("a")
+publisher11.send("b")
+publisher11.send("c")//최근것 반영
+
+print("ㅡㅡㅡㅡzipㅡㅡㅡㅡ")
+
+let publisher12 = PassthroughSubject<Int, Never>()
+let publisher13 = PassthroughSubject<String, Never>()
+
+publisher12.zip(publisher13).sink {
+    print("P1: \($0), P2: \($1)")
+}
+
+publisher12.send(1)
+publisher12.send(2)
+publisher13.send("a")//쌍이 모일때만.
+publisher13.send("b")
 
 
