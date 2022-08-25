62d98a2c40a98e792983f1d76426077ab542a866
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-08-25 15:03:49 +0900

    feat: button image

---
+++ Redux pattern/practice/screenshot/8_25/buttonImage.swift
@@ -0,0 +1,46 @@
+//
+//  buttonImage.swift
+//  Redux pattern
+//
+//  Created by Hankyu Lee on 2022/08/25.
+//
+
+import Foundation
+//    fileprivate var checkButton: UIButton = {
+//        let button = UIButton()//42
+//        button.setWidth(width: 42)
+//        button.setHeight(height: 42)
+////        button.setGradient(color1: .blue, color2: .red, bounds: CGRect(x: 0, y: 0, width: 42, height: 42))
+////        let image = UIImage(systemName: "checkmark.circle.fill")
+////        button.setImage(image, for: .normal)
+////        let imageConfig = UIImage.SymbolConfiguration(pointSize: 22)
+////        button.setPreferredSymbolConfiguration(imageConfig, forImageIn: .normal)
+//
+//        button.tintColor = .white
+//        button.layer.cornerRadius = 21
+//
+////        let gradient: CAGradientLayer = CAGradientLayer()
+////        gradient.colors = [UIColor.red, UIColor.blue]
+////        gradient.locations = [0.0 , 1.0]
+//////        gradient.frame = bounds
+////        gradient.cornerRadius = 21
+////        button.layer.insertSublayer(gradient, at: 0)
+////        button.backgroundColor = .red
+////        print(button.bounds)
+////        button.layer.backgroundColor = UIColor.red.cgColor
+//
+////        button.layer.masksToBounds = true
+////        button.imageView?.backgroundColor = .green
+////        button.imageEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+////        button.clipsToBounds = true
+////
+////
+////        button.setBackgroundImage(image, for: .normal)
+////        button.setImage(image., for: .normal)
+////        button.imageEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+////        button.contentMode = .scaleToFill
+////        button.imageEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+//
+//        button.addTarget(self, action: #selector(toggleCheck), for: .touchUpInside)
+//        return button
+//    }()
